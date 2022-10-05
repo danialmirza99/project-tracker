9c6bf6fa3ea35edc27e9110364d0760bd247e291
danialmirza99 <danialmirza99@gmail.com>
2022-10-05 12:06:09 -0700

    added bootstrap card

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-var timeDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+// Create a header/hero area that welcomes users to the application and 
+// displays the current time and date using Moment.js with `setInterval()`.
+// DONE
+
+var timeDate = moment().format("dddd, MMMM Do YYYY h:mm:ss a");
 $("#time-date").text(timeDate);
+
+let updateTime = function () {
+    let currentTime = moment().format('dddd, MMMM Do YYYY h:mm:ss a')
+    $("#time-date").text(currentTime);
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+// Create a Bootstrap card component explaining the instructions of how 
+// to use the app and a button to open a 
+// [Bootstrap modal dialog](https://getbootstrap.com/docs/4.5/components/modal/).
+// TODO
+
